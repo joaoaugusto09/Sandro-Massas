4edb05b35bd7a272888d266b532087d43c7a6e6f
joaoaugusto09 <joaocarbonera06@gmail.com>
2022-06-09 16:02:00 -0300

    0906 Banco de Dados
    Conexões com o Banco de Dados

---
+++ includes/_banco.php
@@ -0,0 +1,12 @@
+<?php 
+$servername = "localhost";
+$database = "meusite";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Erro ao conectar no Banco: " . mysqli_connect_error());
+}
+?>
